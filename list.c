3f112ab513657a5d93129b02424db05a1217c6c2
Vinod <vinod@segfault.in>
2016-11-03 09:39:12 +0000

    insert_after

--- list.c
+++ list.c
@@ -50,6 +50,40 @@ struct node* insert_last(struct node* h, int data)
 }
 
 /*
+ * cases:
+ * list is empty
+ * node cannot be found
+ * data is the first node
+ * data is a middle node
+ * data is the last node
+ */
+struct node* insert_after(struct node* h, struct node* n, int data)
+{
+	struct node* t = h;
+	if(h == NULL)
+		return h;
+
+	while(t != NULL) {
+		if(t->data == data) {
+			break;
+		}
+		t = t->next;
+	}
+
+	if(t == NULL)
+		return h;
+
+	if(t->next == NULL) {
+		t->next = n;
+	} else {
+		n->next = t->next->next;
+		t->next = n;
+	}
+
+	return h;
+}
+
+/*
  * print list
  */
 void print(struct node* h)
@@ -58,7 +92,7 @@ void print(struct node* h)
 		printf("NULL\n");
 		return;
 	}
-	
+
 	printf("[%d]->", h->data);
 	print(h->next);
 }
@@ -139,7 +173,30 @@ struct node* delete_last(struct node *h)
 	return h;
 }
 
-int
+	void
+print_operations()
+{
+	printf("1.  Print list\n");
+	printf("2.  Reverse list\n");
+	printf("3.  Insert first\n");
+	printf("4.  Insert last\n");
+	printf("5.  Insert at position\n");
+	printf("6.  Insert before position\n");
+	printf("7.  Insert after position\n");
+	printf("8.  Insert after node\n");
+	printf("9.  Insert before node\n");
+	printf("10. Delete first\n");
+	printf("11. Delete last\n");
+	printf("12. Delete at position\n");
+	printf("13. Delete before position\n");
+	printf("14. Delete after position\n");
+	printf("15. Delete after node\n");
+	printf("16. Delete before node\n");
+	printf("17. Reverse list\n");
+	printf("q. Exit\n");
+}
+
+	int
 main(int argc, char *argv[])
 {
 	int i;
@@ -150,16 +207,25 @@ main(int argc, char *argv[])
 
 	print(h);
 
-        h = reverse(h, NULL);
+	h = reverse(h, NULL);
 	print(h);
 
 	h = reverse2(h);
 	print(h);
 
-        h = delete_first(h);
+	h = delete_first(h);
 	print(h);
 
-        h = delete_last(h);
+	h = delete_last(h);
+	print(h);
+
+	h = insert_first(h, 11);
+	print(h);
+
+	h = insert_last(h, 13);
+	print(h);
+
+	h = insert_after(h, newnode(14), 13);
 	print(h);
 
 	return 0;
